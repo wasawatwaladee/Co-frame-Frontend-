c6eec020318596bf63149e6652fb68c3aa76cf90
Kunpichai <Kun_pichai@hotmail.com>
2025-11-25 16:14:18 +0700

    communityPage

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PostContainer() {
   return (
-    <div className=' h-full w-[680px] mx-auto min-h-screen my-3 flex flex-col gap-4 rounded-lg bg-amber-100'>
+    <div className=' w-[680px] mx-auto min-h-screen my-3 flex flex-col gap-4 rounded-lg bg-amber-100'>
         PostContainer
     </div>
   )
