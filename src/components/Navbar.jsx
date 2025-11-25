026c4af0a0db8ad0695eeb34fdad8c2d1ae2640b
wasawatwaladee <wasawatwaladee2@gmail.com>
2025-11-25 15:49:27 +0700

    Add NavLink in NavBar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -65,7 +66,9 @@ const Navbar = () => {
                 d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
               />
             </svg>
-            <span>Login</span>
+            
+
+            <NavLink to={'/login'}>Login</NavLink>
           </a>
         </div>
       </div>
